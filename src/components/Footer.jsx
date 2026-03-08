2c2e547887d84997ef23bb135899986716602bad
Jayeetra Bhattacharjee <66883035+jayeetrab@users.noreply.github.com>
2026-03-08 22:18:01 +0000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,11 +21,10 @@ const Footer = () => {
   className="w-10 h-10 order-1 md:order-none"
 />
 
-        {/* Ikon Sosmed - di tengah di mobile */}
+        {/*Jay mobile */}
         <div className="flex gap-3 order-2 md:order-none">
           <a href="https://github.com/jayeetrab"><i className="ri-github-fill ri-2x"></i></a>
-          <a href="https://www.instagram.com/farisedrikprayoga/"><i className="ri-instagram-fill ri-2x"></i></a>
-          <a href="https://www.youtube.com/@FarisEdrikPrayoga"><i className="ri-youtube-fill ri-2x"></i></a>
+          <a href="https://www.instagram.com/jayhighlens/"><i className="ri-instagram-fill ri-2x"></i></a>
         </div>
 
         {/* Dock - paling bawah di mobile */}
